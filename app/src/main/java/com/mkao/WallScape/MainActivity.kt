9f28e04fb7dfb12e87f16ad96421004ebfb56fb2
mkaocodes <mkaoibrahim@gmail.com>
2024-01-31 10:45:16 +0300

    DynamicColor Support

--- app/src/main/java/com/mkao/WallScape/MainActivity.kt
+++ app/src/main/java/com/mkao/WallScape/MainActivity.kt
@@ -6,38 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mkao.WallScape.ui.theme.GreetingsAppTheme
+import com.mkao.WallScape.ui.theme.WallTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingsAppTheme {
+            WallTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GreetingsAppTheme {
-        Greeting("Android")
     }
 }
